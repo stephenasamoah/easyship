10c1c136d508963fef35fd4ccee550518c25ec7b
Stephen Asamoah <stephenasamoah@users.noreply.github.com>
2019-11-13 17:29:29 +0000

    #### Branding - Updated branding info

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('naya-multistep app is running!');
+    expect(page.getTitleText()).toEqual('easyship app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'naya-multistep'`, () => {
+  it(`should have as title 'easyship'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('naya-multistep');
+    expect(app.title).toEqual('easyship');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('naya-multistep app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('easyship app is running!');
   });
 });
